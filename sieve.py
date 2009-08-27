64588526a5d6a8c920a12ab641bbe13049320ec5
Alex Kritikos <alex@archbook.localdomain>
2009-08-26 21:42:06 -0500

    Sped up my old sieve and made a faster one

--- sieve.py
+++ sieve.py
@@ -1,21 +1,35 @@
 def raw_sieve(m):
     nums = range(m)
     nums[1] = 0
-    i = 0
+    i = 2
     while i**2 < m:
         if nums[i] == 0:
             i += 1
             continue
-        # The next non-zero should be prime
-        p = nums[i]
-
         # Zero out all the multiples of p, starting at the square (optimization)
-        j = p
-        while j * p < m:
-            nums[j * p] = 0
-            j += 1
+        j = i**2
+        while j < m:
+            nums[j] = 0
+            j += i
         i += 1
     return nums
 
 def filtered_sieve(m):
     return [x for x in raw_sieve(m) if x != 0]
+
+def wheel_sieve(m):
+    nums = range(m >> 1)
+    i = 3
+    while i**2 < m:
+        if nums[i >> 1] == 0:
+            i += 2
+            continue
+        # Zero out all the multiples of p, starting at the square (optimization)
+        # Ignore the evens
+        j = i**2
+        I = i << 1
+        while j < m:
+            nums[j >> 1] = 0
+            j += I
+        i += 2
+    return [2] + [n * 2 + 1 for n in nums if n != 0]
