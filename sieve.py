9f5620429bb0b4a6e9fbdaee55b950fa41533be7
Alex Kritikos <alex@archbook.localdomain>
2009-08-26 22:03:06 -0500

    Removed the bitwise operators, as they weren't any faster, ans possibly confusing

--- sieve.py
+++ sieve.py
@@ -18,18 +18,18 @@ def filtered_sieve(m):
     return [x for x in raw_sieve(m) if x != 0]
 
 def wheel_sieve(m):
-    nums = range(m >> 1)
+    nums = range(m / 2)
     i = 3
     while i**2 < m:
-        if nums[i >> 1] == 0:
+        if nums[i / 2] == 0:
             i += 2
             continue
         # Zero out all the multiples of p, starting at the square (optimization)
         # Ignore the evens
-        j = i**2
-        I = i << 1
+        j = i ** 2
+        I = i * 2
         while j < m:
-            nums[j >> 1] = 0
+            nums[j / 2] = 0
             j += I
         i += 2
     return [2] + [n * 2 + 1 for n in nums if n != 0]
