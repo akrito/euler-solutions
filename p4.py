7b35814e4a32057035188e55d735709a70b8f67e
Alex Kritikos <alex@archbook.localdomain>
2009-08-27 02:11:15 -0500

    sped up #4 by running it backwards, exiting early and remembering multiplication is commutative

--- p4.py
+++ p4.py
@@ -1,7 +1,9 @@
 t = 0
-for i in xrange(1, 1000):
-    for j in xrange(1, 1000):
+for i in xrange(999, 0, -1):
+    for j in xrange(999, i - 1, -1):
         k = i * j
+        if k <= t:
+            break
         s = str(k)
         good = True
         for l in xrange(0, len(s) / 2):
@@ -9,6 +11,5 @@ for i in xrange(1, 1000):
                 good = False
                 break
         if good:
-            if k > t:
-                t = k
-                print t
+            t = k
+print t
