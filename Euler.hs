525887b51e35c42d7bce84063fb805984c816aa4
Alex Kritikos <alex@archbook.localdomain>
2009-08-28 16:55:44 -0500

    added inefficient prime and permutation generators

--- Euler.hs
+++ Euler.hs
@@ -17,3 +17,18 @@ isPrime :: (Integral a) => a -> Bool
 isPrime n
     | n < 2     = False
     | otherwise = all ((/= 0) . (mod n)) [2..truncate (sqrt (fromIntegral n))]
+
+-- slow method from http://www.haskell.org/haskellwiki/Prime_numbers
+--slowPrimes :: [Integer]
+showPrimes = 2:3:primes'
+  where
+    1:p:candidates = [6*k+r | k <- [0..], r <- [1,5]]
+    primes'        = p : filter isPrime candidates
+    isPrime n      = all (not . divides n) $ takeWhile (\p -> p*p <= n) primes'
+    divides n p    = n `mod` p == 0
+
+permute ns = _permute (length ns) [] ns
+_permute n l r
+    | length l == n = [l]
+    | otherwise     = foldr (f l r) [] r
+    where f l r d ans = ans ++ _permute n (l ++ [d]) (filter (\x -> x/= d) r)
