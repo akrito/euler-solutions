d70f1faf5818b746dfe744939b7a8010fc9cdd36
Alex Kritikos <alex@archbook.localdomain>
2009-09-03 23:08:41 -0500

    reverted 10 to the fasted haskell solution I found

--- p10.hs
+++ p10.hs
@@ -1,6 +1,4 @@
 module Main where
 import Euler
-import qualified Data.IntSet as I
-main = do
-  primes <- iosieve 100000
-  print $ sum primes
+--import qualified Data.IntSet as I
+main = do print $ sum $ sieve 2000000
